27c5b76ed7f89de619bec0a1cbba9a12232286cd
nguyen-the-anh-vu <vuntavfx17889@funix.edu.vn>
2023-06-24 11:11:19 +0700

    updated

--- funix/urls.py
+++ funix/urls.py
@@ -1,9 +1,8 @@
-from funix.views.problem import ProblemBeta, ProblemListBeta,ProblemCommentsBeta
+from funix.views.problem import ProblemBeta, ProblemListBeta,ProblemCommentsBeta, ProblemDetailsBeta, ProblemSubmitBeta
 from funix.views.problem_data import beta_update_problem_data
 from funix.views.submission import abort_submission_beta, SubmissionTestCaseQueryBeta
 from django.urls import path, include
 from judge.views.widgets import rejudge_submission
-
 
 urlpatterns = [
     path('/problems', ProblemListBeta.as_view(), name='beta_problem_list'),
@@ -12,6 +11,7 @@ urlpatterns = [
         path('/comments', ProblemCommentsBeta.as_view(), name='beta_problem_comments'), 
         path('/submission/<int:submission>', ProblemBeta.as_view(), name='beta_problem'), 
         path('/data', beta_update_problem_data, name='beta_update_problem_data'), 
+        path('/submit', ProblemSubmitBeta.as_view(), name='beta_problem_submit'), 
     ])),
     path('submission/<int:submission>', include([
         path('/abort', abort_submission_beta, name='beta_submission_abort'), 
@@ -19,6 +19,7 @@ urlpatterns = [
     path('widgets/', include([
         path('rejudge', rejudge_submission, name='beta_submission_rejudge'),
         path('submission_testcases', SubmissionTestCaseQueryBeta.as_view(), name='beta_submission_testcases_query'),
+        path('details', ProblemDetailsBeta.as_view(), name='beta_problem_details'),
         ])
     )
 ]
